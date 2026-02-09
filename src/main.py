85c858799a11a7331547f3dae1792b4a7d1fc0ba
choole <nemanjacukic31@gmail.com>
2026-02-09 23:35:20 +0100

    Add docs

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import os
 import shutil
+from sys import argv
 from utils import copy_static
 from gencontent import generate_pages_recursive
 
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 
 def main():
+    basepath = default_basepath
+    if len(argv) > 1:
+        basepath = argv[1]
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -22,7 +28,8 @@ def main():
     generate_pages_recursive(
         dir_path_content,
         template_path,
-        dir_path_public
+        dir_path_public,
+        basepath
     )
 
 
